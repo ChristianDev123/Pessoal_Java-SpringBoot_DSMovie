74d92508b995d43fa652f11b17c146a8e4c40bf4
ChrisDev123 <christian.lima999@gmail.com>
2022-03-10 09:20:07 -0300

    MovieCard

--- frontend/src/pages/Listing/listagem.tsx
+++ frontend/src/pages/Listing/listagem.tsx
@@ -1,8 +1,28 @@
+import MovieCard from "components/MovieCard/MovieCard";
 import Pagination from "components/Pagination/Pagination";
-export default function Listing(){
-    return(
-        <>
-            <Pagination />
-        </>
-    );
+export default function Listing() {
+  return (
+    <>
+      <Pagination />
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3"> {/* quando a largura for pequena o card terá dois cards na página, e quando a largura da página for grande o terá 3 cards na tela */}
+            <MovieCard/>
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3"> {/*mb = margin-bottom */}
+            <MovieCard/>
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3"> 
+            <MovieCard/>
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3"> 
+            <MovieCard/>
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3"> 
+            <MovieCard/>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
